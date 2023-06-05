46b4a9dc36c17f18f3d52759c20a87141e57bc6e
HugoNovais <hugo.o.novais@outlook.com>
2023-06-04 22:28:40 -0300

    sera

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
-mongoose.connect('mongodb+srv://hugobt:teste@cluster0.wxegcih.mongodb.net/test', {useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect('mongodb+srv://hugobt:teste@cluster0.wxegcih.mongodb.net/', {useNewUrlParser: true, useUnifiedTopology: true})
 .then(() => console.log('Conectado ao MongoDB'))
 .catch((err) => console.error(err));
